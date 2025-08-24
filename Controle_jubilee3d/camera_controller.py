bb7e3d72a12b259c62e68ba6f595ca1b5881fca0
jose-2-sales <icraspilum@gmail.com>
2025-08-24 11:24:40 -0300

    Primeira parte readme

--- Controle_jubilee3d/camera_controller.py
+++ Controle_jubilee3d/camera_controller.py
@@ -3,12 +3,43 @@ import numpy as np
 
 
 class camera_tool:
-    def __init__(self, machine=None):
+    """ Classe para controle de uma ferramenta de câmera em um sistema Jubilee ou similar.
+
+    Esta classe encapsula os comandos necessários para instalar, desinstalar
+    e capturar imagens usando uma câmera acoplada à máquina. A movimentação
+    é feita através da interface da máquina, enquanto a captura de imagens
+    utiliza a biblioteca OpenCV.
+
+    Atributos da classe
+    ----------
+    installed : bool
+        Indica se a câmera está instalada no sistema.
+    machine : objeto
+        Interface da máquina controlada (deve implementar métodos como `move_xyz_absolute` e `gcode`).
+    move_velocity : int
+        Velocidade padrão para movimentação do cabeçote durante instalação/desinstalação."""
+
+    def __init__(self, machine):
+        """
+        Inicializa a ferramenta da câmera.
+
+        Parâmetros
+        ----------
+        machine : Instância da Jubilee.
+        """
         self.installed = False
         self.machine = machine
         self.move_velocity = 10000
 
     def install(self):
+        """
+        Instala a ferramenta da câmera na máquina.
+
+        Este método move o cabeçote para as coordenadas específicas
+        necessárias para acoplar a câmera ao sistema Jubilee. As
+        Coodernadas foram descobertas empíricamente e podem ser 
+        alteradas se necessário.
+        """
         self.machine.move_xyz_absolute(y=220, velocity=self.move_velocity)
         self.machine.move_xyz_absolute(x=302, velocity=self.move_velocity)
         self.machine.gcode("G0 U70")
@@ -18,6 +49,12 @@ class camera_tool:
         self.machine.move_xyz_absolute(x=50, y=120, velocity=self.move_velocity)
 
     def uninstall(self):
+        """
+        Remove a ferramenta da câmera da máquina.
+
+        Este método move o cabeçote para as coordenadas específicas
+        necessárias para desacoplar a câmera do sistema Jubilee.
+        """
         self.machine.move_xyz_absolute(y=90, velocity=self.move_velocity)
         self.machine.move_xyz_absolute(x=302, velocity=self.move_velocity)
         self.machine.move_xyz_absolute(y=7, velocity=self.move_velocity)
@@ -26,7 +63,25 @@ class camera_tool:
         self.machine.move_xyz_absolute(x=50, y=120, velocity=self.move_velocity)
         self.machine.gcode("G0 U0")
 
-    def photo(self, filename='captura.jpg',video_index=0):
+    def photo(self, filename='captura.jpg', video_index=0):
+        """
+        Captura uma imagem usando a câmera conectada.
+
+        Este método abre um dispositivo de captura de vídeo via OpenCV,
+        obtém um frame e salva como arquivo de imagem.
+
+        Parâmetros
+        ----------
+        filename : str, opcional
+            Nome do arquivo de saída da imagem capturada (padrão: 'captura.jpg').
+        video_index : int, opcional
+            Índice do dispositivo de captura de vídeo (padrão: 0).
+
+        Retorna
+        -------
+        None
+            Não há retorno. Se a captura falhar, o método apenas encerra silenciosamente.
+        """
         cap = cv2.VideoCapture(video_index)
         if not cap.isOpened():
             return
@@ -37,6 +92,4 @@ class camera_tool:
             return
 
         cv2.imwrite(filename, frame)
-
         cap.release()
-
--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# raspilum_jubilee
+## Science Jubilee
+
+Science Jubilee é uma plataforma de automação laboratorial de código aberto, baseada no sistema Jubilee, uma plataforma de movimento multiferramenta extensível, originalmente projetada como uma impressora 3D com troca automática de cabeças de ferramenta [**[1]**](https://jubilee3d.com/index.php?title=Main_Page). 
+
+![Jubilee](/Imagens Readme/550px-Jubilee_v2.1.2.png)
+
+A science-jubilee adapta essa infraestrutura para aplicações científicas, permitindo automação de experimentos com ferramentas como pipetas, câmeras, sondas e manuseio de amostras. A documentação oficial está disponível no site Science Jubilee Docs, que reúne guias de início rápido, construção da plataforma, desenvolvimento, referências da API e contribuições da comunidade. A plataforma é flexível e serve como base para diferentes aplicações de automação laboratorial, desde pipetagem e manipulação de amostras até aquisição de imagens.  [**[2]**](https://science-jubilee.readthedocs.io/en/latest/index.html)
+
+### Exemplos de uso publicados
+
+1. O artigo "A high-throughput workflow for the synthesis of CdSe nanocrystals using a sonochemical materials acceleration platform", publicado em 2023, apresenta uma plataforma aceleradora de materiais para síntese de nanocristais de CdSe. O sistema combinou o Jubilee com um equipamento de automação de líquidos da Opentrons OT-2, ultrassom e espectrometria, permitindo 625 condições experimentais [**[3]**](https://pubs.rsc.org/en/content/articlehtml/2023/dd/d3dd00033h).  
+
+2. O artigo "The Duckbot: A system for automated imaging and manipulation of duckweed" mostra um sistema para manipulação automatizada de lentilha d'água, uma planta quye serve. Foram implementadas ferramentas como, pipeta, câmeras, loops de inoculação e módulos Python para controle via Jupyter notebooks, permitindo experimentos de crescimento com manipulação e análise automatizadas [**[4]**](https://journals.plos.org/plosone/article?id=10.1371/journal.pone.0296717).  
+
+
+## Jubilee RaspIlum
+
+
+
+
+### Referências
+
+[1] Jubilee 3D. Jubilee wiki [Internet]. Disponível em: https://jubilee3d.com/index.php?title=Main_Page  
+[2] Science Jubilee. Documentation [Internet]. Disponível em: https://science-jubilee.readthedocs.io/en/latest/index.html  
+[3] Telford J, Newton G, Shields R, Mohr S, Handley J, Thielbeer F, et al. A sonochemical materials acceleration platform for the synthesis of CdSe nanocrystals. Digital Discovery. 2023;2(3):856-865.  
+[4] Babl L, Holten V, Reuter M, Schoof H. Duckbot: a modular open-source platform for automated experiments with duckweed. PLoS One. 2024;19(2):e0296717.  
+
+
